1bb1366f6a18948da838a8f01946604c29f6938d
razorrahul123 <rahul.kr.sushant@gmail.com>
2020-08-07 05:38:47 +0530

    Updates

---
+++ precache-manifest.bc1d7a14553b6564e28595a8f624ca70.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60698744470ee70083611f3e1641eb36",
+    "url": "/flickr-react/index.html"
+  },
+  {
+    "revision": "e1c5e88c9a6c5cb716ff",
+    "url": "/flickr-react/static/css/2.1e4dac1d.chunk.css"
+  },
+  {
+    "revision": "70c6a838d14ebd22e4da",
+    "url": "/flickr-react/static/css/main.7e77d499.chunk.css"
+  },
+  {
+    "revision": "e1c5e88c9a6c5cb716ff",
+    "url": "/flickr-react/static/js/2.fd9ea1ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flickr-react/static/js/2.fd9ea1ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c6a838d14ebd22e4da",
+    "url": "/flickr-react/static/js/main.de5545a6.chunk.js"
+  },
+  {
+    "revision": "9e102b41137a765a1658",
+    "url": "/flickr-react/static/js/runtime-main.9e051356.js"
+  },
+  {
+    "revision": "d486b3f56a31bf0738938b4f001a8b39",
+    "url": "/flickr-react/static/media/flickr-logo.d486b3f5.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/flickr-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/flickr-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/flickr-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/flickr-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/flickr-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
